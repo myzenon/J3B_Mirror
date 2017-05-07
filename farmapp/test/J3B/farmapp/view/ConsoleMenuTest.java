858441a662564db2e6b182d08f838582fa580639
shadowangle <shadowangle1222@hotmail.com>
2017-05-07 20:56:45 +0700

    consoleMenu Test

--- farmapp/test/J3B/farmapp/view/ConsoleMenuTest.java
+++ farmapp/test/J3B/farmapp/view/ConsoleMenuTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.*;
 
 import java.io.ByteArrayInputStream;
 
+import J3B.farmapp.model.impl.Fruit;
 import org.junit.Test;
 
 import J3B.farmapp.controller.GameController;
@@ -11,13 +12,81 @@ import J3B.farmapp.model.impl.Plant;
 import J3B.farmapp.util.Input;
 
 public class ConsoleMenuTest {
-	ConsoleMenu consoleMenu;
+    @Test
+    public void viewPlants() throws Exception {
+        GameController gc = new GameController();
+        consoleMenu = new ConsoleMenu(gc);
+        gc.plantSeed(gc.getSeeds().get(0));
+        for (int i = 0; i < 5 ; i++) {
+            gc.sleep();
+        }
+        Input.setSystemInput(new ByteArrayInputStream("1".getBytes()));
+        consoleMenu.viewPlants();
+
+
+    }
+    @Test
+    public void waterPlant() throws Exception {
+        GameController gc = new GameController();
+        consoleMenu = new ConsoleMenu(gc);
+        gc.plantSeed(gc.getSeeds().get(0));
+        Input.setSystemInput(new ByteArrayInputStream("1".getBytes()));
+        consoleMenu.viewInventory();
+    }
+
+    @Test
+    public void harvestFruit() throws Exception {
+        GameController gc = new GameController();
+        consoleMenu = new ConsoleMenu(gc);
+        gc.plantSeed(gc.getSeeds().get(1));
+        for (int i = 0; i < 100 ; i++) {
+            gc.sleep();
+        }
+        Input.setSystemInput(new ByteArrayInputStream("1".getBytes()));
+        consoleMenu.harvestFruit();
+    }
+
+    @Test
+    public void viewInventory() throws Exception {
+        GameController gc = new GameController();
+        consoleMenu = new ConsoleMenu(gc);
+        gc.getPlayer().getInventory().addItem(new Fruit("Apple",10));
+        gc.getPlayer().getInventory().addItem(new Fruit("Orange",10));
+        gc.getPlayer().getInventory().addItem(new Fruit("Mango",10));
+        Input.setSystemInput(new ByteArrayInputStream("3 1".getBytes()));
+        consoleMenu.viewInventory();
+        Input.setSystemInput(new ByteArrayInputStream("3 2".getBytes()));
+        consoleMenu.viewInventory();
+        Input.setSystemInput(new ByteArrayInputStream("3 3".getBytes()));
+        consoleMenu.viewInventory();
+    }
+
+
+
+
+
+
+    ConsoleMenu consoleMenu;
 
     @Test
     public void showAll() throws Exception {
     	GameController gc = new GameController();
     	consoleMenu = new ConsoleMenu(gc);
-        Input.setSystemInput(new ByteArrayInputStream("7 1".getBytes()));
+        gc.getPlayer().getInventory().addItem(new Fruit("Apple",10));
+        gc.getPlayer().getInventory().addItem(new Fruit("Orange",10));
+        gc.getPlayer().getInventory().addItem(new Fruit("Mango",10));
+    	 Input.setSystemInput(new ByteArrayInputStream("7 1".getBytes()));
+        Input.setSystemInput(new ByteArrayInputStream("2 1 1 1 3 1".getBytes()));
+       Input.setSystemInput(new ByteArrayInputStream("4 1".getBytes()));
+       Input.setSystemInput(new ByteArrayInputStream("5 3 1".getBytes()));
+        Input.setSystemInput(new ByteArrayInputStream("5 3 2".getBytes()));
+        Input.setSystemInput(new ByteArrayInputStream("5 3 3".getBytes()));
+
+        consoleMenu.showAll();
+        for (int i = 0; i < 2 ; i++) {
+            gc.sleep();
+        }
+        Input.setSystemInput(new ByteArrayInputStream("6 1".getBytes()));
         consoleMenu.showAll();
     }
     
